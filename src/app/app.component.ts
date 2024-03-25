0531b22c5a276a30d2fd21066863b00badfa8ac5
darrrev <dar.dev@yahoo.com>
2024-03-26 00:03:38 +0800

    list-box v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'list-box';
+
+  listBoxOne = ["One", "Two", "Three", "Four", "Five", "Six"]; 
+  listBoxTwo = [];
+  selectedIndex;
+
+  constructor() {
+
+  }
+
+  selectedItem(event, index) {
+    event.currentTarget.style.borderLeft = "5px solid #5d9cc9";
+    this.selectedIndex = index;
+  }
+
+  toRight() {
+    this.listBoxTwo.push(this.listBoxOne[this.selectedIndex]);
+    this.listBoxOne.splice(this.selectedIndex, 1);
+  }
+
+  toLeft() {
+    this.listBoxOne.push(this.listBoxTwo[this.selectedIndex]);
+    this.listBoxTwo.splice(this.selectedIndex, 1);
+  }
+
+  allRight() {
+    this.listBoxTwo = this.listBoxOne;
+    this.listBoxOne = [];
+  }
+
+  allLeft() {
+    this.listBoxOne = this.listBoxTwo;
+    this.listBoxTwo = [];
+  }
 }
